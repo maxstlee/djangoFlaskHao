4d0e8c4d06382f383c3aad2b4b6947747f5da3a1
maxstlee <maxstlee@gmail.com>
2018-07-07 15:56:39 -0400

    Add files via upload

---
+++ Django/main_page/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path, include
+from django.contrib import admin
+
+from . import views
+# from user_example import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('about_the_project/', views.about_the_project, name='about_the_project'),
+    path('sample_dashboard/', views.sample_dashboard, name='sample_dashboard'),
+    path('recipe_examples/', views.recipe_examples, name='recipe_examples'),
+    path('survey_example/', views.survey_example, name='survey_example'),
+    path('user_login/', views.user_login, name='user_login'),
+    path('user_signup/', views.user_signup, name='user_signup')
+]
---
+++ Django/main_page/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+
+def index(request):
+    return render(request, 'main_page/index.html')
+
+def about_the_project(request):
+    return render(request, 'main_page/about_the_project.html')
+
+def sample_dashboard(request):
+    return render(request, 'main_page/sample_dashboard.html')
+
+def recipe_examples(request):
+    return render(request, 'main_page/recipe_examples.html')
+
+def survey_example(request):
+    return render(request, 'main_page/survey_example.html')
+
+def login_index(request):
+    return render(request, 'user_example/templates/user_example/index.html')
+
+def user_login(request):
+    return render(request, 'user_example/templates/registration/login.html')
+
+def user_signup(request):
+    return render(request, 'user_example/templates/registration/register.html')
